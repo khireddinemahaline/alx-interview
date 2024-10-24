0beb109e7eb82dc9bbadde25eb763292e2369f93
khireddinemahaline <khirozr@gmail.com>
2024-10-24 17:08:54 -0400

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
-""" log parsing
-    to write a script that reads stdin line by line and computes metrics
-    input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
 """
-import sys
+log parsing
+"""
 
-def parsing_log():
-    """parse stdin"""
-    status_codes = {
-        "200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0
-    }
-    file_size = 0  # total file size
-    count = 0  # total number of lines read
+import sys
+import re
+
+
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                file_size += int(data[-1])
-            except Exception:
-                pass
-            try:
-                if data[-2] in status_codes:
-                    status_codes[data[-2]] += 1
-            except Exception:
-                pass
-            if count % 10 == 0:
-                print("File size: {}".format(file_size))
-                for key, value in sorted(status_codes.items()):
-                    if value:
-                        print("{}: {}".format(key, value))
-    except KeyboardInterrupt:
-        pass
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
     finally:
-        print("File size: {}".format(file_size))
-        for key, value in sorted(status_codes.items()):
-            if value:
-                print("{}: {}".format(key, value))  # print status code
-        raise
-    
+        output(log)
