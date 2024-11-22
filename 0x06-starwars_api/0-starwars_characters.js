7b3aeef40697b1045ec85895a98695e0042fd96b
khireddinemahaline <khirozr@gmail.com>
2024-11-22 21:25:48 +0100

    alx

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+// Function to get all characters
+function getStarWarsCharacters () {
+  // API URL
+  const url = 'https://swapi-api.alx-tools.com/api/people/';
+  // Send request to the API
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error('Error occurred:', error);
+    } else if (response.statusCode !== 200) {
+      console.error('HTTP Error:', response.statusCode);
+    } else {
+      // Parse JSON response
+      const data = JSON.parse(body);
+      // Print characters
+      data.results.forEach(character => {
+        console.log(character.name);
+      });
+    }
+  });
+}
+
+getStarWarsCharacters();
